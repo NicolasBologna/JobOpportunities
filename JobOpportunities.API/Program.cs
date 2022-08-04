b064f5c6464089ad61fff46e3e47f7bc56ac6a94
Nicolas Bologna <nicolasandresbologna@hotmail.com>
2022-08-04 18:13:31 -0300

    reorganize program.cs

--- JobOpportunities.API/Program.cs
+++ JobOpportunities.API/Program.cs
@@ -15,9 +15,38 @@ using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
 
+var connectionString = builder.Configuration.GetConnectionString("JobOpportunitiesContextConnection") ?? throw new InvalidOperationException("Connection string 'JobOpportunitiesContextConnection' not found.");
+builder.Services.AddDbContext<JobOpportunitiesContext>(options =>
+    options.UseSqlServer(connectionString));
+
+//builder.Services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
+//    .AddEntityFrameworkStores<JobOpportunitiesContext>();
+
+builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+builder.Services.AddScoped(typeof(IReadRepository<>), typeof(ReadOnlyRepository<>));
+#region SpecificRepositories
+builder.Services.AddScoped<IJobOfferRepository, JobOfferRepository>();
+builder.Services.AddScoped<ICandidatesRepository, CandidatesRepository>();
+#endregion
+
+builder.Services.AddControllers(options =>
+        options.Filters.Add<ApiExceptionFilterAttribute>() //Agregar validaciones globales a los controladores
+    ).AddJsonOptions(joptions =>
+        joptions.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles)
+    .AddFluentValidation();
+
+builder.Services.Configure<ApiBehaviorOptions>(options =>
+    options.SuppressModelStateInvalidFilter = true); //Modificamos la configuración default de ApiBehaviorOptions para que la validación automatica que agrega [ApiController] ya no entre en vigor, ya que queremos usar nuestra validación custom.
+builder.Services.AddValidatorsFromAssembly(typeof(GetJobOfferQuery).Assembly);
+builder.Services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehaviour<,>));
+
+builder.Services.AddMediatR(typeof(JobOfferController).Assembly, typeof(GetJobOfferQuery).Assembly);
+
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
 builder.Services.AddSwaggerGen(setupAction =>
 {
-    setupAction.AddSecurityDefinition("ConsultaAlumnosApiBearerAuth", new OpenApiSecurityScheme() //Esto va a permitir usar swagger con el token.
+    setupAction.AddSecurityDefinition("TBD", new OpenApiSecurityScheme() //Esto va a permitir usar swagger con el token.
     {
         Type = SecuritySchemeType.Http,
         Scheme = "Bearer",
@@ -32,80 +61,11 @@ builder.Services.AddSwaggerGen(setupAction =>
                 Reference = new OpenApiReference
                 {
                     Type = ReferenceType.SecurityScheme,
-                    Id = "ConsultaAlumnosApiBearerAuth" } //Tiene que coincidir con el id seteado arriba en la definici�n
+                    Id = "TBD" } //Tiene que coincidir con el id seteado arriba en la definici�n
                 }, new List<string>() }
     });
 });
-
-builder.Services.AddControllers();
-
-
-var connectionString = builder.Configuration.GetConnectionString("JobOpportunitiesContextConnection") ?? throw new InvalidOperationException("Connection string 'JobOpportunitiesContextConnection' not found.");
-builder.Services.AddDbContext<JobOpportunitiesContext>(options =>
-    options.UseSqlServer(connectionString));
-
-//builder.Services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
-//    .AddEntityFrameworkStores<JobOpportunitiesContext>();
-
-builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
-builder.Services.AddScoped(typeof(IReadRepository<>), typeof(ReadOnlyRepository<>));
-
-#region SpecificRepositories
-builder.Services.AddScoped<IJobOfferRepository, JobOfferRepository>();
-builder.Services.AddScoped<ICandidatesRepository, CandidatesRepository>();
-#endregion
-
-builder.Services.AddControllers(options =>
-        options.Filters.Add<ApiExceptionFilterAttribute>() //Agregar validaciones globales a los controladores
-    ).AddJsonOptions(joptions =>
-        joptions.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles)
-    .AddFluentValidation();
-
-builder.Services.Configure<ApiBehaviorOptions>(options =>
-    options.SuppressModelStateInvalidFilter = true); //Modificamos la configuración default de ApiBehaviorOptions para que la validación automatica que agrega [ApiController] ya no entre en vigor, ya que queremos usar nuestra validación custom.
-
-builder.Services.AddValidatorsFromAssembly(typeof(GetJobOfferQuery).Assembly);
-builder.Services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehaviour<,>));
-
-/*
-builder.Services.AddAuthentication("Bearer")
-    .AddJwtBearer(options =>
-    {
-        options.Events = new JwtBearerEvents
-        {
-            OnTokenValidated = context =>
-            {
-                //TODO
-                //ApplicationUser
-
-                var userMachine = context.HttpContext.RequestServices.GetRequiredService<UserManager<ApplicationUser>>();
-                var user = userMachine.GetUserAsync(context.HttpContext.User);
-                if (user == null)
-                    context.Fail("UnAuthorized");
-                return Task.CompletedTask;
-            }
-        };
-        options.TokenValidationParameters = new()
-        {
-            ValidateIssuer = true,
-            ValidateAudience = true,
-            ValidateIssuerSigningKey = true,
-            ValidIssuer = builder.Configuration["Authentication:Issuer"],
-            ValidAudience = builder.Configuration["Authentication:Audience"],
-            IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(builder.Configuration["Authentication:SecretForKey"]))
-        };
-    }
-);*/
-
-builder.Services.AddMediatR(typeof(JobOfferController).Assembly, typeof(GetJobOfferQuery).Assembly);
-
-builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
-
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
