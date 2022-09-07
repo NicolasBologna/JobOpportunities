ba82839d59f9537f065d8650aeff80a92f73a142
Nicolas Bologna <nicolasandresbologna@hotmail.com>
2022-09-07 12:16:39 -0300

    add not found component

--- JobOpportunities.SPA/src/app/app-routing.module.ts
+++ JobOpportunities.SPA/src/app/app-routing.module.ts
@@ -3,11 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { JobOffersComponent } from './jobOffers/jobOffers.component';
 import { HomeComponent } from './home/home.component';
+import { NotFoundComponent } from './error-pages/not-found/not-found.component';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'jobOffers', component: JobOffersComponent },
-  { path: '**', redirectTo: '/home' },
+  { path: '404', component: NotFoundComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: '**', redirectTo: '/404', pathMatch: 'full' },
 ];
 
 @NgModule({
